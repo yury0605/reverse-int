d809795723ef440ddbde0597ffde5072b1b7942d
Yury <sendtopepper@gmail.com>
2023-02-02 13:06:17 +0300

    done task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+let text = n.toString();
+let i = 0;
+let result = '';
+while(i < text.length){
+    result = text[i] + result;
+    i = i + 1;
+} 
+return result;
 }
+
